2c2612b985ae4745bd9e9f0598a1d6cbf3a90a7f
Pedro Augusto Lopes Gomes <pedroaugustolopesgomes@gmail.com>
2025-02-27 12:55:48 -0300

    feat: add db seed script

---
+++ prisma/migrations/20250227155109_update_schema/migration.sql
@@ -0,0 +1,75 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[slug]` on the table `Restaurant` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `customerCpf` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `customerName` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Changed the type of `consumptionMethod` on the `Order` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `price` on the `Product` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- CreateEnum
+CREATE TYPE "ConsumptionMethod" AS ENUM ('TAKEAWAY', 'DINE_IN');
+
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "OrderStatus" ADD VALUE 'PAYMENT_CONFIRMED';
+ALTER TYPE "OrderStatus" ADD VALUE 'PAYMENT_FAILED';
+
+-- DropForeignKey
+ALTER TABLE "MenuCategory" DROP CONSTRAINT "MenuCategory_restaurantId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Order" DROP CONSTRAINT "Order_restaurantId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderProduct" DROP CONSTRAINT "OrderProduct_orderId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderProduct" DROP CONSTRAINT "OrderProduct_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Product" DROP CONSTRAINT "Product_menuCategoryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Product" DROP CONSTRAINT "Product_restaurantId_fkey";
+
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "customerCpf" TEXT NOT NULL,
+ADD COLUMN     "customerName" TEXT NOT NULL,
+DROP COLUMN "consumptionMethod",
+ADD COLUMN     "consumptionMethod" "ConsumptionMethod" NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "price",
+ADD COLUMN     "price" DOUBLE PRECISION NOT NULL;
+
+-- DropEnum
+DROP TYPE "OrderConsumptionMethod";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Restaurant_slug_key" ON "Restaurant"("slug");
+
+-- AddForeignKey
+ALTER TABLE "MenuCategory" ADD CONSTRAINT "MenuCategory_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_menuCategoryId_fkey" FOREIGN KEY ("menuCategoryId") REFERENCES "MenuCategory"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProduct" ADD CONSTRAINT "OrderProduct_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProduct" ADD CONSTRAINT "OrderProduct_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
